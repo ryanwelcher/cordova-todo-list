d8d50bc20658b9dde15e5a35c05f0ac8c27a93ec
Ryan Welcher <ryan.welcher@10up.com>
2016-02-24 21:38:29 -0500

    Adding grunt tasks to watch and run prepare when files change

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		exec: {
+			prepare: {
+				cmd: 'cordova prepare'
+			}
+		},
+		watch : {
+			scripts: {
+				files: ['www/**/*.*'],
+				tasks: ['exec:prepare'],
+				options: {
+					spawn: false
+				}
+			},
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+};
